bcd3eea1d06fd16002c085e9c39536f269d8ba74
Justin Le <mstksg@gmail.com>
2013-07-29 01:55:48 -0700

    persistent models set up

--- src/Web/Blog/Models.hs
+++ src/Web/Blog/Models.hs
@@ -1,3 +1,33 @@
+{-# LANGUAGE FlexibleContexts             #-} 
+{-# LANGUAGE GADTs                        #-} 
+{-# LANGUAGE QuasiQuotes                  #-} 
+{-# LANGUAGE TemplateHaskell              #-} 
+{-# LANGUAGE TypeFamilies                 #-} 
+{-# LANGUAGE TypeSynonymInstances         #-} 
+{-# LANGUAGE GeneralizedNewtypeDeriving   #-} 
+{-# LANGUAGE EmptyDataDecls               #-} 
+
 module Web.Blog.Models () where
 
+import Database.Persist
+import Database.Persist.Sqlite
+import Database.Persist.TH
+import Data.Time
+import Data.Text
 
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Entry
+    title       String
+    content     Text
+    createdAt   UTCTime
+    postTime    UTCTime
+    deriving    Show
+
+Tag
+    label String
+
+EntryTag
+    entryId          EntryId
+    tagId            TagId
+    UniqueEntryTag   entryId   tagId
+|]
