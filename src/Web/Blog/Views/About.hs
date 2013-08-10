0a69c84381f17958b99b2df43cd9a5e9956f5cae
Justin Le <mstksg@gmail.com>
2013-08-09 18:22:57 -0700

    rearranging css files

--- src/Web/Blog/Views/About.hs
+++ src/Web/Blog/Views/About.hs
@@ -5,18 +5,13 @@ module Web.Blog.Views.About (
   ) where
 
 import Text.Blaze.Html5                      ((!))
-import Web.Blog.Render
 import Web.Blog.Types
-import Web.Blog.Util
-import qualified Data.Foldable as Fo         (forM_)
-import qualified Data.Text                   as T
 import qualified Text.Blaze.Html5            as H
 import qualified Text.Blaze.Html5.Attributes as A
-import qualified Text.Blaze.Internal         as I
 
 viewAbout :: SiteRender H.Html
 viewAbout = return $
-  H.section ! A.class_ "tile" $ do
+  H.section ! A.class_ "tile single-page" $ do
     H.header $
       H.h1 "About me"
 
--- src/Web/Blog/Views/NotFound.hs
+++ src/Web/Blog/Views/NotFound.hs
@@ -2,16 +2,31 @@
 
 module Web.Blog.Views.NotFound (viewNotFound) where
 
+import Web.Blog.Render
 import Web.Blog.Types
 import qualified Text.Blaze.Html5            as H
 import qualified Text.Blaze.Html5.Attributes as A
+import qualified Text.Blaze.Internal         as I
 
 viewNotFound :: SiteRender H.Html
-viewNotFound = 
+viewNotFound = do
+  homeUrl <- renderUrl "/"
+  archiveUrl <- renderUrl "/entries"
+
   return $ 
-    H.section H.! A.class_ "tile" $ do
+    H.section H.! A.class_ "tile single-page" $ do
+
       H.header $
         H.h1 "Not Found"
-      H.div $
-        H.p "The page you were looking for was not found.  Sorry!"
+
+      H.hr
+
+      H.div H.! A.class_ "main-content" $
+        H.p $ do
+          "The page you were looking for was not found.  Sorry!  "
+          "Maybe try going to the "
+          H.a H.! A.href (I.textValue homeUrl) $ "homepage"
+          " or checking out the "
+          H.a H.! A.href (I.textValue archiveUrl) $ "archives"
+          "?"
 
