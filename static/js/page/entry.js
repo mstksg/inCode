9bd19d376cdea36034f45511a38fabe3b6cfb954
Justin Le <mstksg@gmail.com>
2013-08-18 01:34:51 -0700

    link to top fixed

--- static/js/page/entry.js
+++ static/js/page/entry.js
@@ -1,30 +1,43 @@
 
 $(document).ready(function() {
-  $('#toc').toc({
-      'selectors': 'h1,h2,h3,h4', //elements to use as headings
-      'container': '.main-content', //element to find all selectors in
-      'smoothScrolling': true, //enable or disable smooth scrolling on click
-      'prefix': 'sec', //prefix for anchor tags and class names
-      // 'onHighlight': function(el) {}, //called when a new section is highlighted
-      'highlightOnScroll': false, //add class to heading that is currently in focus
-      // 'highlightOffset': 100, //offset to trigger the next headline
-      'anchorName': function(i, heading, prefix) { //custom function for anchor name
-          return prefix+i;
-      },
-      'headerText': function(i, heading, $heading) { //custom function building the header-item text
-          return $heading.text();
-      },
-  'itemClass': function(i, heading, $heading, prefix) { // custom function for item class
-    return $heading[0].tagName.toLowerCase();
-  }
-  });
+    $('#toc').toc({
+        'selectors': 'h1,h2,h3,h4', //elements to use as headings
+        'container': '.main-content', //element to find all selectors in
+        'smoothScrolling': true, //enable or disable smooth scrolling on click
+        'prefix': 'sec', //prefix for anchor tags and class names
+        // 'onHighlight': function(el) {}, //called when a new section is highlighted
+        'highlightOnScroll': false, //add class to heading that is currently in focus
+        // 'highlightOffset': 100, //offset to trigger the next headline
+        'anchorName': function(i, heading, prefix) { //custom function for anchor name
+            return prefix+i;
+        },
+        'headerText': function(i, heading, $heading) { //custom function building the header-item text
+            return $heading.text();
+        },
+        'itemClass': function(i, heading, $heading, prefix) { // custom function for item class
+            return $heading[0].tagName.toLowerCase();
+        }
+    });
 
-  // $('.main-content').children('h2,h3,h4,h5').wrap('<div>').append('<a href="#entry-title">top</a>');
-  $('.main-content').children('h2,h3,h4,h5').append('<a href="#entry-title" class="top-link">top</a>');
 
-  if ($('#toc li').length > 0) {
-      $('.contents-container').show();
-  }
+    $('.main-content').children('h2,h3,h4,h5').append('<a href="#entry-title" class="top-link">top</a>');
+
+    $('.top-link') .bind('click', function(e) {
+        scrollTo(e);
+    });
+
+    if ($('#toc li').length > 0) {
+        $('.contents-container').show();
+    }
 
 });
 
+var scrollTo = function(e) {
+    e.preventDefault();
+    var elScrollTo = $(e.target).attr('href');
+    var $el = $(elScrollTo);
+
+    $('body,html').animate({ scrollTop: $el.offset().top }, 400, 'swing', function() {
+        location.hash = elScrollTo;
+    });
+};
