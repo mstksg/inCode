e3e47f602df91cecec8ed0a9d446bfa041ae93a6
Justin Le <mstksg@gmail.com>
2013-08-19 03:43:36 -0700

    more advanced info in empty feed

--- src/Web/Blog/Views/Feed.hs
+++ src/Web/Blog/Views/Feed.hs
@@ -8,23 +8,45 @@ import Web.Blog.Types
 import qualified Data.Text      as T
 import qualified Data.Text.Lazy as L
 import Network.URI
+import Data.Time
 
 viewFeed :: SiteRender L.Text
-viewFeed = return $ L.pack $ showXML $ rssToXML feedRss
+viewFeed = return $ L.pack $ showXML $ rssToXML $ feedRss []
 
-feedRss :: RSS
-feedRss = RSS
+feedRss :: [String] -> RSS
+feedRss entryInfos = RSS
             feedTitle
-            feedURI
+            feedLink
             feedDescription
-            []
+            feedChannel
             []
   where
     siteDataString r = T.unpack $ r siteData
     feedTitle = siteDataString siteDataTitle
-    feedURI = URI "http:" (Just auth) "" "" ""
+    feedLink = URI "http:" (Just auth) "" "" ""
       where
         auth = URIAuth "" host ""
         host = siteDataString siteDataSiteHost
     feedDescription = siteDataString siteDataDescription
-    -- feedAuthor = siteDataString (authorInfoEmail . siteDataAuthorInfo)
+    feedAuthorEmail = siteDataString (authorInfoEmail . siteDataAuthorInfo)
+    feedAuthorName = siteDataString (authorInfoName . siteDataAuthorInfo)
+    feedAuthor = concat [feedAuthorEmail, " (", feedAuthorName, ")"]
+    feedChannel =
+      [ Language "en"
+      , Copyright "Copyright 2013 Justin Le"
+      , ManagingEditor feedAuthor
+      , WebMaster feedAuthor
+      -- , ChannelPubDate ""
+      , LastBuildDate testTime
+      -- , ChannelCategory (Just "dmoz") "Computers/Programming/Internet/Personal_Pages"
+      -- , ChannelCategory (Just "syndic8") ""
+      , Generator "rss-3000.2.0.2 (Bas van Dijk)"
+      -- , TTL 60
+      -- , Image "" "" "" "" "" ""
+      , SkipHours []
+      , SkipDays []
+      ]
+
+
+testTime :: UTCTime
+testTime = UTCTime (ModifiedJulianDay 10352) 19542
