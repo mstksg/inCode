7154de72ff841b3173db3038b602a9b4c3a1f631
Justin Le <mstksg@gmail.com>
2013-09-01 20:24:32 -0700

    new update entry title mechanism, with slug update and linking and stuff.

--- src/Web/Blog/Models/Entry.hs
+++ src/Web/Blog/Models/Entry.hs
@@ -4,17 +4,18 @@ module Web.Blog.Models.Entry  where
 
 -- import qualified Database.Esqueleto       as E
 import Control.Applicative                   ((<$>))
-import Control.Monad
+-- import Control.Monad
 import Control.Monad.IO.Class                (liftIO)
 import Control.Monad.Loops                   (firstM)
 import Data.List                             (groupBy)
-import Data.Maybe                            (isNothing, fromJust, isJust)
+import Data.Maybe                            (fromJust, isJust)
 import Data.Time
 import Web.Blog.Models
 import Web.Blog.Models.Types
 import Web.Blog.SiteData
 import Web.Blog.Types
 import Web.Blog.Util
+import qualified Data.Foldable as Fo         (forM_)
 import qualified Data.Text                   as T
 import qualified Database.Persist.Postgresql as D
 import qualified Text.Blaze.Html5            as H
@@ -28,20 +29,28 @@ ledeMax = appPrefsLedeMax $ siteDataAppPrefs siteData
 insertEntry :: Entry -> D.SqlPersistM (Maybe (D.Key Entry))
 insertEntry entry = do
   entryKey <- D.insertUnique entry
-  when (isJust entryKey) $
-    insertSlug $ D.Entity (fromJust entryKey) entry
+  Fo.forM_ entryKey $ \k ->
+    insertSlug k $ entryTitle entry
   return entryKey
-
 
 insertEntry_ :: Entry -> D.SqlPersistM ()
 insertEntry_ entry = do
   entryKey <- D.insert entry
-  insertSlug $ D.Entity entryKey entry
+  insertSlug entryKey $ entryTitle entry
 
-insertSlug :: D.Entity Entry -> D.SqlPersistM ()
-insertSlug (D.Entity entryKey entry) = do
-  slugText <- genEntrySlug slugLength (entryTitle entry)
-  D.insert_ $ Slug entryKey slugText True
+updateEntryTitle :: D.Key Entry -> T.Text -> D.SqlPersistM ()
+updateEntryTitle eKey newTitle = do
+    D.update eKey [EntryTitle D.=. newTitle]
+    D.updateWhere [SlugEntryId D.==. eKey] [SlugIsCurrent D.=. False]
+    insertSlug eKey newTitle
+
+insertSlug :: D.Key Entry -> T.Text -> D.SqlPersistM ()
+insertSlug eKey title = do
+  slugText <- genEntrySlug slugLength eKey title
+  oldSlug <- D.getBy $ UniqueSlug slugText
+  case oldSlug of
+    Just (D.Entity sKey _) -> D.update sKey [SlugIsCurrent D.=. True]
+    Nothing -> D.insert_ $ Slug eKey slugText True
 
 entryPandoc :: Entry -> P.Pandoc
 entryPandoc = P.readMarkdown (P.def P.ReaderOptions) . T.unpack . entryContent
@@ -66,9 +75,8 @@ entryLedePandoc entry = P.Pandoc m ledeBs
 
 
 
--- TODO: Find way to generalize and merge this with genTagSlug
-genEntrySlug :: Int -> T.Text -> D.SqlPersistM T.Text
-genEntrySlug w t = do
+genEntrySlug :: Int -> D.Key Entry -> T.Text -> D.SqlPersistM T.Text
+genEntrySlug w k t = do
   let
     baseSlug = genSlug w t
   base <- D.getBy $ UniqueSlug baseSlug
@@ -82,8 +90,8 @@ genEntrySlug w t = do
   where
     isFresh :: T.Text -> D.SqlPersistM Bool
     isFresh s = do
-      found <- D.getBy $ UniqueSlug s
-      return $ isNothing found
+      found <- D.selectList [ SlugSlug D.==. s, SlugEntryId D.!=. k ] []
+      return $ null found
 
 -- TODO: separate changeSlug function to be able to re-double back on old
 -- names
