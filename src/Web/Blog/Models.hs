f40838c876cc379f526d64db84169711eeda6908
Justin Le <mstksg@gmail.com>
2013-09-02 18:13:00 -0700

    rearranged structure so that models and instances are in different files

--- src/Web/Blog/Models.hs
+++ src/Web/Blog/Models.hs
@@ -1,74 +1,8 @@
-{-# LANGUAGE FlexibleContexts             #-} 
-{-# LANGUAGE GADTs                        #-} 
-{-# LANGUAGE QuasiQuotes                  #-} 
-{-# LANGUAGE TemplateHaskell              #-} 
-{-# LANGUAGE TypeFamilies                 #-} 
-{-# LANGUAGE TypeSynonymInstances         #-} 
-{-# LANGUAGE GeneralizedNewtypeDeriving   #-} 
-{-# LANGUAGE EmptyDataDecls               #-} 
-{-# LANGUAGE FlexibleInstances            #-}
 
-module Web.Blog.Models  where
+module Web.Blog.Models
+  ( module Web.Blog.Models.Models
+  ) where
 
-import Data.Time
-import Database.Persist.TH
-import Web.Blog.Models.Types
--- import Data.Maybe (maybe)
-import qualified Data.Text   as T
+import Web.Blog.Models.Models
+import Web.Blog.Models.Instances()
 
-share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
-
-Entry
-    title       T.Text
-    content     T.Text
-    createdAt   UTCTime Maybe
-    postedAt    UTCTime Maybe
-    modifiedAt  UTCTime Maybe
-    identifier  T.Text Maybe
-
-    UniqueEntryTitle title
-
-Tag
-    label           T.Text
-    type_           TagType
-    description     T.Text Maybe
-    slug            T.Text
-
-    UniqueLabelType label type_
-    UniqueSlugType  slug  type_
-    deriving        Eq Show Read
-
-EntryTag
-    entryId          EntryId
-    tagId            TagId
-
-    UniqueEntryTag   entryId   tagId
-    deriving         Show
-
-Slug
-    entryId    EntryId Eq
-    slug       T.Text
-    isCurrent  Bool
-
-    UniqueSlug slug
-    deriving Show
-
-RemovedEntry
-    title       T.Text
-    content     T.Text
-    createdAt   UTCTime Maybe
-    postedAt    UTCTime Maybe
-    modifiedAt  UTCTime Maybe
-    removedAt   UTCTime
-    identifier  T.Text Maybe
-|]
-
-instance Show Entry where
-  show (Entry t _ cA pA _ i) = concat
-    [ show t
-    , " ("
-    , maybe "" ((++ ", ") . show) i
-    , maybe "" ((++ ", ") . show) cA
-    , maybe "no post date" (("posted " ++) . show) pA
-    , ")"
-    ]
---
+++ src/Web/Blog/Models/Instances.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE FlexibleInstances    #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Web.Blog.Models.Instances where
+
+import Web.Blog.Models.Models
+
+instance Show Entry where
+  show (Entry t _ cA pA _ i) = concat
+    [ show t
+    , " ("
+    , maybe "" ((++ ", ") . show) i
+    , maybe "" ((++ ", ") . show) cA
+    , maybe "no post date" (("posted " ++) . show) pA
+    , ")"
+    ]
