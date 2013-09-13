201abe9e77d8df5928fcaea871f3768a25dec8b7
Justin Le <mstksg@gmail.com>
2013-09-12 20:52:56 -0700

    changed to record syntax for config

--- src/Config/SiteData.hs
+++ src/Config/SiteData.hs
@@ -8,31 +8,36 @@ import qualified Data.Text as T (concat)
 siteData :: SiteData
 siteData =
   SiteData
-    "in Code"
-    authorInfo
-    (T.concat
+    { siteDataTitle           = "in Code"
+    , siteDataAuthorInfo      = authorInfo
+    , siteDataDescription     = description
+    , siteDataCopyright       = "2013 Justin Le"
+    , siteDataHostConfig      = hostConfig
+    , siteDataDeveloperAPIs   = developerAPIs
+    , siteDataAppPrefs        = appPrefs
+    , siteDataDatabaseConfig  = databaseConfig
+    , siteDataSiteEnvironment = SiteEnvironmentDevelopment
+    }
+  where
+    description = T.concat
       [ "Weblog of Justin Le, covering his various adventures in "
       , "programming and explorations in the vast worlds of computation, "
-      , "physics, and knowledge."] )
-    "2013 Justin Le"
-    hostConfig
-    developerAPIs
-    appPrefs
-    databaseConfig
-    SiteEnvironmentDevelopment
-  where
+      , "physics, and knowledge."]
     authorInfo = AuthorInfo
-                   "Justin Le"
-                   "mstksg@gmail.com"
-                   "https://plus.google.com/107705320197444500140"
-                   "mstksg"
-                   "907281"
-                   "107705320197444500140"
-                   "mstksg"
-                   "lejustin"
+                   { authorInfoName      = "Justin Le"
+                   , authorInfoEmail     = "mstksg@gmail.com"
+                   , authorInfoRel       = "https://plus.google.com/107705320197444500140"
+                   , authorInfoFacebook  = "mstksg"
+                   , authorInfoTwitterID = "907281"
+                   , authorInfoGPlus     = "107705320197444500140"
+                   , authorInfoGithub    = "mstksg"
+                   , authorInfoLinkedIn  = "lejustin"
+                   }
+                
     hostConfig = HostConfig
-                   "blog-dev.jle0.com"
-                   (Just 4288)
+                   { hostConfigHost = "blog-dev.jle0.com"
+                   , hostConfigPort = Just 4288
+                   }
 
     developerAPIs = DeveloperAPIs
                       ("UA-443711-7", "jle0.com")
@@ -41,14 +46,15 @@ siteData =
                       "ra-520df7c304b817b9"
                       "justinleblogdevelopment"
     appPrefs = AppPrefs
-                 8
-                 5
-                 2
-                 15
-    databaseConfig = Just $ DatabaseConfig
-                       "localhost"
-                       "test_blog"
-                       "blog-test"
-                       "blog-testblog-test"
-                       4432
-
+                 { appPrefsSlugLength  = 8
+                 , appPrefsHomeEntries = 5
+                 , appPrefsLedeMax     = 2
+                 , appPrefsFeedEntries = 15
+                 }
+    databaseConfig = Just DatabaseConfig
+                       { databaseConfigHost     = "localhost"
+                       , databaseConfigName     = "test_blog"
+                       , databaseConfigUser     = "blog-test"
+                       , databaseConfigPassword = "blog-testblog-test"
+                       , databaseConfigPort     = 4432
+                       }
