e6a270a8a1a1bb88673211e7b1957f3443f8bb45
Justin Le <mstksg@gmail.com>
2013-09-13 11:18:20 -0700

    basic deployment script

--- src/Development/Blog/Util/Compass.hs
+++ src/Development/Blog/Util/Compass.hs
@@ -5,9 +5,12 @@ import System.Cmd
 import Control.Monad
 import Config.SiteData
 import Web.Blog.Types
+import System.Directory
 
 compileCompass :: IO ()
-compileCompass = void $ rawSystem "compass" ["compile", "-e" , environment]
+compileCompass = do
+  createDirectoryIfMissing True "tmp/static/css"
+  void $ rawSystem "compass" ["compile", "-e" , environment]
   where
     environment =
       case siteDataSiteEnvironment siteData of
