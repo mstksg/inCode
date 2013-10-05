19fc8ae2da601b28f89c8c1e38173c377c301dec
Justin Le <mstksg@gmail.com>
2013-10-04 19:06:54 -0700

    removing leading H1 if any

--- src/Development/Blog/Util/LoadTags.hs
+++ src/Development/Blog/Util/LoadTags.hs
@@ -2,19 +2,22 @@
 module Development.Blog.Util.LoadTags (loadTags) where
 
 -- import Control.Monad.IO.Class
--- import qualified Text.Pandoc                  as P
--- import qualified Text.Pandoc.Readers.Markdown as PM
-import Control.Applicative                       ((<$>))
+-- import qualified Data.Foldable             as Fo
+import Control.Applicative                    ((<$>))
 import Control.Monad
-import Data.List                                 (isPrefixOf)
-import System.Directory                          (getDirectoryContents, doesDirectoryExist)
-import System.FilePath                           ((</>), takeBaseName)
+import Data.List                              (isPrefixOf)
+import Data.Maybe
+import System.Directory                       (getDirectoryContents, doesDirectoryExist)
+import System.FilePath                        ((</>), takeBaseName)
 import Web.Blog.Database
 import Web.Blog.Models
 import Web.Blog.Models.Types
-import qualified Data.Foldable                   as Fo
-import qualified Data.Text                       as T
-import qualified Database.Persist.Postgresql     as D
+import qualified Data.Text                    as T
+import qualified Data.Traversable             as Tr
+import qualified Database.Persist.Postgresql  as D
+import qualified Text.Pandoc                  as P
+import qualified Text.Pandoc.Builder          as P
+import qualified Text.Pandoc.Readers.Markdown as PM
 
 loadTags :: FilePath -> IO ()
 loadTags tagsDir = do
@@ -34,18 +37,44 @@ loadTags tagsDir = do
         else
           return []
 
-  forM_ (zip tagFiless [GeneralTag ..]) $ \(tagFiles, tagType) ->
-    mapM_ (processTagFile tagType) tagFiles
+  foundTags <- forM (zip tagFiless [GeneralTag ..]) $ \(tagFiles, tagType) ->
+    mapM (processTagFile tagType) tagFiles
 
-processTagFile :: TagType -> FilePath -> IO ()
+  runDB $ clearOrphanTags $ catMaybes $ join foundTags
+
+processTagFile :: TagType -> FilePath -> IO (Maybe (D.Key Tag))
 processTagFile tagType tagFile = do
-    contents <- T.pack <$> readFile tagFile
+    (P.Pandoc _ (b:bs), _) <- readMarkdown <$> readFile tagFile
+    
+    let
+      contents = T.pack $ renderBlocks $
+        case b of
+          P.Header 1 _ _ -> bs
+          _ -> b:bs
 
     let
       slug = T.pack $ takeBaseName tagFile
 
     tag <- runDB $ D.getBy $ UniqueSlugType slug tagType
 
-    Fo.forM_ tag $ \(D.Entity tagKey tagVal) -> do
+    Tr.forM tag $ \(D.Entity tagKey tagVal) -> do
       print tagVal
       runDB $ D.update tagKey [ TagDescription D.=. Just contents ]
+      return tagKey
+
+  where
+    readMarkdown :: String -> (P.Pandoc, [String])
+    readMarkdown = PM.readMarkdownWithWarnings (P.def P.ReaderOptions)
+    renderBlocks bs =
+      P.writeMarkdown basicWriterOptions $ P.doc $ P.fromList bs
+
+basicWriterOptions :: P.WriterOptions
+basicWriterOptions = (P.def P.WriterOptions)
+                       { P.writerReferenceLinks = True
+                       }
+
+clearOrphanTags :: [D.Key Tag] -> D.SqlPersistM ()
+clearOrphanTags tagKeys =
+    D.updateWhere
+      [ TagId D./<-. tagKeys ]
+      [ TagDescription D.=. Nothing ]
