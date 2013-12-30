ee7d92ccbb909e2f816a1f86b8f6b6ada4ad32ff
Justin Le <mstksg@gmail.com>
2013-12-29 16:21:09 -0800

    packages and stuff

--- src/Main.hs
+++ src/Main.hs
@@ -3,7 +3,7 @@
 -- import Network.Wai
 -- import Web.Blog.Database
 import Control.Applicative                  ((<$>))
-import Control.Monad.IO.Class
+-- import Control.Monad.IO.Class
 import Development.Blog.Util
 import Network.Wai.Middleware.Headers
 import Network.Wai.Middleware.RequestLogger
@@ -16,19 +16,19 @@ import Web.Scotty
 
 main :: IO ()
 main = do
+  startupHelpers
+
   port <- case hostConfigPort $ siteDataHostConfig siteData of
     Just p' -> return p'
     Nothing -> read <$> getEnv "PORT"
 
   scotty port $ do
 
-  liftIO startupHelpers
+    middleware logStdoutDev
+    middleware $ addHeaders [("Cache-Control","max-age=86400")]
+    middleware $ staticPolicy (noDots >-> addBase "static")
+    middleware $ staticPolicy (noDots >-> addBase "tmp/static")
+    middleware $ addHeaders [("Cache-Control","max-age=900")]
 
-  middleware logStdoutDev
-  middleware $ addHeaders [("Cache-Control","max-age=86400")]
-  middleware $ staticPolicy (noDots >-> addBase "static")
-  middleware $ staticPolicy (noDots >-> addBase "tmp/static")
-  middleware $ addHeaders [("Cache-Control","max-age=900")]
-
-  route
+    route
 
