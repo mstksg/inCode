28d7262230704efcc4febf83424a7c836e78ef18
Justin Le <mstksg@gmail.com>
2013-12-29 17:28:49 -0800

    304 not modifieds on static things

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+-- import Control.Monad.IO.Class
+-- import Debug.Trace
 -- import Network.Wai
 -- import Web.Blog.Database
+import Config.SiteData
 import Control.Applicative                  ((<$>))
--- import Control.Monad.IO.Class
+import Data.ByteString                      (ByteString, isPrefixOf)
 import Development.Blog.Util
+import Network.Wai                          (rawPathInfo)
+import Network.Wai.Middleware.Cache
 import Network.Wai.Middleware.Headers
 import Network.Wai.Middleware.RequestLogger
 import Network.Wai.Middleware.Static
 import System.Environment                   (getEnv)
 import Web.Blog.Routes
-import Config.SiteData
 import Web.Blog.Types
 import Web.Scotty
 
@@ -25,10 +29,38 @@ main = do
   scotty port $ do
 
     middleware logStdoutDev
-    middleware $ addHeaders [("Cache-Control","max-age=86400")]
+    -- middleware $ addHeaders [("Cache-Control","max-age=86400")]
+    middleware headerETag
+    middleware $ cache cacheBackend
     middleware $ staticPolicy (noDots >-> addBase "static")
     middleware $ staticPolicy (noDots >-> addBase "tmp/static")
     middleware $ addHeaders [("Cache-Control","max-age=900")]
 
     route
+
+cacheBackend :: CacheBackend
+cacheBackend app req =
+  case lookupETag req of
+    Just _  ->
+      if anyPrefixes toCache && not (anyPrefixes toNotCache)
+        then
+          return Nothing
+        else
+          Just <$> app req
+    Nothing -> Just <$> app req
+  where
+    anyPrefixes = any (`isPrefixOf` rawPathInfo req)
+
+toCache :: [ByteString]
+toCache = [
+    "/css"
+  , "/favicon.ico"
+  , "/font"
+  , "/img"
+  , "/js"
+  , "/robots.txt"
+  ]
+
+toNotCache :: [ByteString]
+toNotCache = [ "/img/entries" ]
 
