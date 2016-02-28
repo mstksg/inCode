5cb160b67ffe55ef3e7210acebc4348149e4bf33
Justin Le <justin@jle.im>
2016-02-28 02:42:57 -0800

    home set up, things are starting to make sense

--- app/Site.hs
+++ app/Site.hs
@@ -4,20 +4,27 @@
 
 import           Blog.Types
 import           Control.Exception
+import           Control.Monad
+import           Control.Monad.Trans.Maybe
 import           Data.Default
 import           Data.Foldable
 import           Data.List
+import           Data.Maybe
+import           Data.Ord
+import           Data.Time.Format
+import           Data.Time.LocalTime
+import           Data.Traversable
 import           Hakyll
 import           Hakyll.Web.Redirect
 import           Hakyll.Web.Sass
 import           System.FilePath
 import           Text.Jasmine
 import           Text.Sass
-import qualified Data.Map                as M
-import qualified Data.Text               as T
-import qualified Data.Text.Lazy          as TL
-import qualified Data.Text.Lazy.Encoding as TL
-import qualified Data.Yaml               as Y
+import qualified Data.Map                  as M
+import qualified Data.Text                 as T
+import qualified Data.Text.Lazy            as TL
+import qualified Data.Text.Lazy.Encoding   as TL
+import qualified Data.Yaml                 as Y
 
 main :: IO ()
 main = do
@@ -47,7 +54,7 @@ main = do
                    Nothing -> gsubRoute "copy/entries/" (\_ -> "entry/ident/")
                                 `composeRoutes`
                                 setExtension ""
-                   Just s  -> constRoute ("entry" </> s) 
+                   Just s  -> constRoute ("entry" </> s)
           compile $ pandocCompiler
           -- compile copyFileCompiler
           -- compile $ pandocCompiler
@@ -55,12 +62,33 @@ main = do
           --     >>= loadAndApplyTemplate "templates/default.html" postCtx
           --     >>= relativizeUrls
 
+      homePag <- buildPaginateWith mkPage "copy/entries/*" (\i -> fromFilePath ("home" </> show i))
+      preprocess $ print homePag
+      paginateRules homePag $ \i p -> do
+        preprocess $ print i *> print p
+        route idRoute
+        compile copyFileCompiler
+
       forM_ confBlobs $ \b -> do
         match "code-samples/**" $ do
           route   $ gsubRoute ".hs" (\_ -> ".hs.html")
           let f p | ".hs.html" `isSuffixOf` p = dropEnd 5 p
                   | otherwise                 = p
           compile $ redirectCompiler (T.pack . (T.unpack b </>) . f . toFilePath)
+  where
+    mkPage :: MonadMetadata m => [Identifier] -> m [[Identifier]]
+    mkPage ids = do
+      withDates <- fmap catMaybes
+                 . forM ids $ \i -> runMaybeT $ do
+        dString <- MaybeT $ getMetadataField i "date"
+        d <- maybe mzero return
+           $ parseTime defaultTimeLocale "%Y/%m/%d %X" dString
+        return (d :: LocalTime, i)
+      let sorted = map snd
+                 . sortBy (comparing fst)
+                 $ withDates
+      return $ paginateEvery 4 sorted
+
 
 compressJsCompiler :: Compiler (Item String)
 compressJsCompiler = fmap f <$> getResourceString
