f8ce99c3799daf28f28aed27c5652069b1c438da
Justin Le <justin@jle.im>
2016-03-20 02:14:53 -0700

    completed port of entry scripts

--- app-purescript/Entry.purs
+++ app-purescript/Entry.purs
@@ -1,5 +1,6 @@
 module Entry where
 
+import Control.Alt
 import Control.Bind
 import Control.Monad
 import Control.Monad.Eff
@@ -9,6 +10,8 @@ import Control.Monad.Eff.Console.Unsafe
 import Control.Monad.Eff.Ref
 import Control.Monad.Maybe.Trans
 import Control.Monad.State.Trans
+import Control.Monad.Writer.Trans
+import Control.Plus
 import DOM                              as D
 import DOM.Event.EventTarget            as D
 import DOM.Event.EventTypes             as D
@@ -24,10 +27,18 @@ import DOM.Node.NodeList                as DNL
 import DOM.Node.NodeType                as D
 import DOM.Node.ParentNode              as D
 import DOM.Node.Types                   as D
+import Data.Array                       as A
+import Data.Bitraversable
+import Data.Char.Unicode
+import Data.Either
 import Data.Foldable
-import Data.List
+import Data.Generic
+import Data.Lens.Getter
+import Data.List                        as L
 import Data.Maybe
+import Data.Monoid
 import Data.Nullable
+import Data.String
 import Data.Traversable
 import Data.Tuple
 import Prelude
@@ -45,6 +56,7 @@ main = do
       appendTopLinks doc
       setupSourceLink doc
       setupAsides doc
+      processCodeBlocks doc
       log "Goodbye!"
 
   where
@@ -144,6 +156,115 @@ setupAsides doc = do
 
       toggleAll
 
+data LinkSpec = LS { source      :: Maybe String
+                   , interactive :: Maybe String
+                   }
+
+derive instance genericLS :: Generic LinkSpec
+
+instance showLS :: Show LinkSpec where
+  show = gShow
+
+instance semigroupLS :: Semigroup LinkSpec where
+  append (LS s1) (LS s2)
+    = LS { source:      s1.source      <|> s2.source
+         , interactive: s1.interactive <|> s2.interactive
+         }
+
+instance monoidLS :: Monoid LinkSpec where
+  mempty = LS { source: Nothing, interactive: Nothing }
+
+-- assumption: only one <code> per <pre>
+processCodeBlocks
+    :: forall e.
+       D.Document
+    -> Eff (dom :: D.DOM, console :: CONSOLE | e) Unit
+processCodeBlocks doc = do
+    blks <- D.querySelectorAll ".main-content code.sourceCode"
+              $ D.documentToParentNode doc
+    flip traverseNodeList_ blks \blk -> do
+      let blkN = D.elementToNode blk
+      lines <- D.childNodes blkN
+      lSpec <- execWriterT
+           <<< ((<=< childNodes') <<< traverseNodeList_) (pullLinkSpec blkN)
+             $ blkN
+      chompWhitespace blkN
+      genLinkBox lSpec blkN
+      colorPrompt blkN
+  where 
+    childNodes' = liftEff <<< D.childNodes
+    pullLinkSpec
+        :: D.Node
+        -> D.Element
+        -> WriterT LinkSpec (Eff (dom :: D.DOM, console :: CONSOLE | e)) Unit
+    pullLinkSpec blk line = do
+      let lineN = D.elementToNode line
+      linec <- liftEff $ D.textContent lineN
+      for_ prefHandlers $ \(Tuple pref handler) -> do
+        for_ (pref `stripPrefix` linec) $ \stuff -> do
+          liftEff $ do
+            D.setNodeValue "" lineN
+            D.removeChild lineN blk
+          case handler stuff of
+            Left s  -> tell $ LS { source: Just s , interactive: Nothing }
+            Right s -> tell $ LS { source: Nothing, interactive: Just s  }
+    prefHandlers = [ Tuple "-- source: "      Left
+                   , Tuple "-- interactive: " Right
+                   ]
+    chompWhitespace :: forall e'. D.Node -> Eff (dom :: D.DOM | e') Unit
+    chompWhitespace blk = go
+      where
+        go = do
+          fc' <- toMaybe <$> D.firstChild blk
+          for_ fc' \fc -> do
+            isWhitespace <- all isSpace <<< toCharArray <$> D.textContent fc
+            when isWhitespace do
+              D.removeChild fc blk
+              go
+    genLinkBox :: forall e'. LinkSpec -> D.Node -> Eff (dom :: D.DOM | e') Unit
+    genLinkBox (LS s) blk = void <<< runMaybeT $ do
+      _   <- maybe empty return $ s.source <|> s.interactive
+      pre <- nMaybeT $ D.parentNode blk
+      liftEff $ do
+        linkBox <- D.createElement "div" doc
+        let linkBoxN = D.elementToNode linkBox
+        D.setClassName "code-link-box" linkBox
+        D.insertBefore linkBoxN blk pre
+        let boxes = A.mapMaybe (\(Tuple u i) -> (_ `Tuple` i) <$> u)
+                  $ [ Tuple s.source
+                            (Tuple "code-source-link"      "View full source")
+                    , Tuple s.interactive
+                            (Tuple "code-interactive-link" "Interactive"     )
+                    ]
+        for_ boxes \(Tuple u (Tuple cls txt)) -> do
+          link <- D.createElement "a" doc
+          let linkN = D.elementToNode link
+          D.setAttribute "href" u link
+          D.setClassName cls link
+          D.setTextContent txt linkN
+          D.appendChild linkN linkBoxN
+          return unit
+
+        cList <- D.classList linkBox
+        DDTL.add ["hide"] cList
+
+        let preET' = D.elementToEventTarget <$> nodeToElement pre
+        for_ preET' \preET -> do
+          onE D.mouseenter preET <<< D.eventListener $ \_ ->
+            DDTL.remove ["hide"] cList
+          onE D.mouseleave preET <<< D.eventListener $ \_ ->
+            DDTL.add ["hide"] cList
+    colorPrompt :: D.Node -> Eff (dom :: D.DOM, console :: CONSOLE | e) Unit
+    colorPrompt blk = void <<< runMaybeT $ do
+      fc   <- nMaybeT $ D.firstChild blk
+      pr   <- nMaybeT $ D.parentElement blk
+      liftEff $ do
+        tc <- D.textContent fc
+        let isPrompt = A.mapMaybe (_ `stripPrefix` tc) ["λ", "ghci"]
+        case L.toList isPrompt of
+          L.Cons _ _ -> DDTL.add ["code-block-prompt"] =<< D.classList pr
+          L.Nil      -> return unit
+
 
 
 -- | Helpers
@@ -151,11 +272,11 @@ setupAsides doc = do
 fromNodeList
     :: forall e.
        D.NodeList
-    -> Eff (dom :: D.DOM | e) (List D.Element)
+    -> Eff (dom :: D.DOM | e) (L.List D.Element)
 fromNodeList nl = do
     l <- DNL.length nl
-    ns <- traverse (_ `DNL.item` nl) (0 .. (l - 1))
-    return $ mapMaybe (nodeToElement <=< toMaybe) ns
+    ns <- traverse (_ `DNL.item` nl) (0 L... (l - 1))
+    return $ L.mapMaybe (nodeToElement <=< toMaybe) ns
 
 traverseNodeList_
     :: forall e m. MonadEff (dom :: D.DOM | e) m
@@ -206,3 +327,4 @@ nodeToElement n =
     case D.nodeType n of
       D.ElementNode -> Just (U.unsafeCoerce n)
       _             -> Nothing
+
