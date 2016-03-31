f908371487bf02a5ac77af50f58930ef6c79e37d
Justin Le <justin@jle.im>
2016-03-31 00:57:10 -0700

    temporarily removing stumbleupon because of no https

--- src/Blog/View/Social.hs
+++ src/Blog/View/Social.hs
@@ -17,10 +17,10 @@ viewSocialShare =
     H.aside ! A.class_ "social-buttons" $ do
       addThisLine
       H.div ! A.class_ "custom-social-buttons" $ do
-        H.div ! A.class_ "custom-social-button" $
-          H.customParent (H.textTag "su:badge")
-            ! H.customAttribute "layout" "1"
-            $ mempty
+        -- H.div ! A.class_ "custom-social-button" $
+        --   H.customParent (H.textTag "su:badge")
+        --     ! H.customAttribute "layout" "1"
+        --     $ mempty
         H.div ! A.class_ "custom-social-button" $
           H.a
             ! A.href "http://www.reddit.com/submit"
