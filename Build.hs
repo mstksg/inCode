5863554dc6054ffbf2601839b79464d4d37d7a6b
Justin Le <justin@jle.im>
2018-05-25 01:44:07 -0700

    fix shake

--- Build.hs
+++ Build.hs
@@ -1,16 +1,17 @@
 #!/usr/bin/env stack
 -- stack --install-ghc runghc --package shake --package jle-utils --package lens
 
-import Control.Lens hiding        ((<.>))
-import Control.Lens.Cons
-import Control.Monad.IO.Class
-import Data.Char
-import Data.Foldable
-import Data.List
-import Data.String
-import Development.Shake
-import Development.Shake.FilePath
-import JUtils.GHPages
+import           Control.Lens hiding        ((<.>))
+import           Control.Lens.Cons
+import           Control.Monad.IO.Class
+import           Data.Char
+import           Data.Foldable
+import           Data.List
+import           Data.String
+import           Development.Shake
+import           Development.Shake.FilePath
+import           JUtils.GHPages
+import qualified Data.Text                  as T
 
 opts = shakeOptions { shakeFiles     = "_build"
                     , shakeVersion   = "1.0"
@@ -25,41 +26,40 @@ psExes :: [String]
 psExes = ["entry"]
 
 main :: IO ()
-main = do
-    shakeArgs opts $ do
-      want ["build"]
+main = shakeArgs opts $ do
+    want ["build"]
 
-      "build" ~> do
-        need (psReq <$> psExes)
-        unit $ cmd "stack run -- blog-build" "build"
-        liftIO $ updatePages "_site" Nothing
+    "build" ~> do
+      need (psReq <$> psExes)
+      unit $ cmd "stack run -- blog-build" "build"
+      liftIO $ updatePages "_site" Nothing (Just (T.pack "blog.jle.im"))
 
-      "rebuild" ~> do
-        need ["purescript"]
-        unit $ cmd "stack run -- blog-build" "rebuild"
-        liftIO $ updatePages "_site" Nothing
+    "rebuild" ~> do
+      need ["purescript"]
+      unit $ cmd "stack run -- blog-build" "rebuild"
+      liftIO $ updatePages "_site" Nothing (Just (T.pack "blog.jle.im"))
 
-      "bower_components/.installed" %> \t -> do
-        need ["bower.json"]
-        unit $ cmd "bower" "install"
-        cmd "touch" t
+    "bower_components/.installed" %> \t -> do
+      need ["bower.json"]
+      unit $ cmd "bower" "install"
+      cmd "touch" t
 
-      "purescript" ~> do
-        need (psReq <$> psExes)
+    "purescript" ~>
+      need (psReq <$> psExes)
 
-      "_purescript/*.js" %> \out -> do
-        let exName = over _head toUpper $ takeBaseName out
-        need [ "bower_components/.installed"
-             , "app-purescript" </> exName <.> "purs"
-             ]
-        unit $ cmd "pulp build"
-                   "--main" exName
-                   "--src-path" "app-purescript"
-                   "--to" out
+    "_purescript/*.js" %> \out -> do
+      let exName = over _head toUpper $ takeBaseName out
+      need [ "bower_components/.installed"
+           , "app-purescript" </> exName <.> "purs"
+           ]
+      unit $ cmd "pulp build"
+                 "--main" exName
+                 "--src-path" "app-purescript"
+                 "--to" out
 
-      "clean" ~> do
-        removeFilesAfter "_build" ["//*"]
-        unit $ cmd "bower cache" "clean"
-        removeFilesAfter "bower_components" ["//*"]
-        removeFilesAfter "_purescript" ["//*"]
-        unit $ cmd "stack run -- blog-build" "clean"
+    "clean" ~> do
+      removeFilesAfter "_build" ["//*"]
+      unit $ cmd "bower cache" "clean"
+      removeFilesAfter "bower_components" ["//*"]
+      removeFilesAfter "_purescript" ["//*"]
+      unit $ cmd "stack run -- blog-build" "clean"
