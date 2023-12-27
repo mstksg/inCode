dfe064d51f5bbb7337900651216184725e3dbfbf
justin <justin@jle.im>
2023-12-27 10:24:02 +0000

    begin nixification

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "inCode";
+  inputs = {
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [
+          haskellNix.overlay
+          (final: prev: {
+            inCode = final.haskell-nix.project' {
+              name = "inCode";
+              src = ./.;
+              compiler-nix-name = "ghc884";
+              shell = {
+                withHoogle = false;
+                tools = {
+                  cabal = { };
+                  # hlint = { };
+                  # haskell-language-server = { };
+                };
+              };
+            };
+          })
+        ];
+        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+        flake = pkgs.inCode.flake { };
+      in
+      flake
+      // { packages.default = flake.packages."inCode:exe:inCode"; }
+    );
+}
+
