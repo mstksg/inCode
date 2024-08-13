7ab61f481bbaf76b867f5586b301c5acad6d7841
justin <justin@jle.im>
2024-08-12 20:56:25 -0700

    looks like i have something happy

--- code-samples/type-levels/Level6.hs
+++ code-samples/type-levels/Level6.hs
@@ -18,8 +18,6 @@ import Data.Type.Ord
 import GHC.TypeNats
 import Unsafe.Coerce
 
--- import Level5 (LTE (..), Nat (..), SNat (..), isLTE)
-
 insertSortedList :: Ord a => a -> [a] -> [a]
 insertSortedList x = \case
   [] -> [x]
@@ -45,40 +43,27 @@ cmpNatConst :: forall p p' n m a b. (KnownNat n, KnownNat m) => p n a -> p' m b 
 cmpNatConst _ _ = cmpNat (Proxy @n) (Proxy @m)
 
 data DecideInsert :: Nat -> Nat -> Type where
-  DIHere :: (n <= m, Min n m ~ n) => DecideInsert n m
-  DIThere :: (m <= n, Min n m ~ m) => DecideInsert n m
+  DIZ :: (n <= m, Min n m ~ n) => DecideInsert n m
+  DIS :: (m <= n, Min n m ~ m) => DecideInsert n m
 
 decideInsert :: forall a b. (KnownNat a, KnownNat b) => DecideInsert a b
 decideInsert = case cmpNat (Proxy @a) (Proxy @b) of
-  LTI -> DIHere
-  EQI -> DIHere
+  LTI -> DIZ
+  EQI -> DIZ
   GTI -> case cmpNat (Proxy @b) (Proxy @a) of
-    LTI -> DIThere
+    LTI -> DIS
     GTI -> error "absurd"
 
-decideInsert' ::
-  forall a b p p' x y. (KnownNat a, KnownNat b) => p a x -> p' b y -> DecideInsert a b
-decideInsert' _ _ = decideInsert @a @b
-
-sConsEither ::
+sConsMin ::
   forall q r n a.
   (KnownNat q, KnownNat r, n <= q, n <= r) =>
   Entry n a ->
   Sorted (Min q r) a ->
   Sorted n a
-sConsEither = case decideInsert @q @r of
-  DIHere -> SCons :: Entry n a -> Sorted q a -> Sorted n a
-  DIThere -> SCons :: Entry n a -> Sorted r a -> Sorted n a
-
-sConsEither' ::
-  forall q r n p p' x y a.
-  (KnownNat q, KnownNat r, n <= q, n <= r) =>
-  p q x ->
-  p' r y ->
-  Entry n a ->
-  Sorted (Min q r) a ->
-  Sorted n a
-sConsEither' _ _ = sConsEither @q @r
+sConsMin = case cmpNat (Proxy @q) (Proxy @r) of
+  LTI -> SCons :: Entry n a -> Sorted q a -> Sorted n a
+  EQI -> SCons :: Entry n a -> Sorted q a -> Sorted n a
+  GTI -> SCons :: Entry n a -> Sorted r a -> Sorted n a
 
 flipMin :: forall a b. (KnownNat a, KnownNat b) => Min a b :~: Min b a
 flipMin = case cmpNat (Proxy @a) (Proxy @b) of
@@ -93,12 +78,12 @@ flipMin = case cmpNat (Proxy @a) (Proxy @b) of
 insertSorted ::
   forall n m a. (KnownNat n, KnownNat m) => Entry n a -> Sorted m a -> Sorted (Min n m) a
 insertSorted x = \case
-  SSingle y -> case decideInsert' x y of
-    DIHere -> SCons x (SSingle y)
-    DIThere -> SCons y (SSingle x)
-  SCons @q y ys -> case decideInsert' x y of
-    DIHere -> SCons x (SCons y ys)
-    DIThere -> sConsEither' x ys y (insertSorted x ys)
+  SSingle y -> case decideInsert @n @m of
+    DIZ -> SCons x (SSingle y)
+    DIS -> SCons y (SSingle x)
+  SCons @q y ys -> case decideInsert @n @m of
+    DIZ -> SCons x (SCons y ys)
+    DIS -> sConsMin @n @q y $ insertSorted x ys
 
 mergeSorted ::
   forall n m a.
@@ -112,13 +97,31 @@ mergeSorted = \case
     SSingle y ->
       gcastWith (flipMin @n @m) $
         insertSorted y (SCons x xs)
-    SCons @r y ys -> case decideInsert' x y of
-      DIHere -> sConsEither' xs y x $ mergeSorted xs (SCons y ys)
-      DIThere -> sConsEither' x ys y $ mergeSorted (SCons x xs) ys
+    SCons @r y ys -> case decideInsert @n @m of
+      DIZ -> sConsMin @q @m x $ mergeSorted xs (SCons y ys)
+      DIS -> sConsMin @n @r y $ mergeSorted (SCons x xs) ys
+
+popSorted ::
+  Sorted n a ->
+  (Entry n a -> r) ->
+  (forall m. (KnownNat m, n <= m) => Entry n a -> Sorted m a -> r) ->
+  r
+popSorted = \case
+  SSingle x -> \f _ -> f x
+  SCons x xs -> \_ g -> g x xs
 
 data SomeSorted a = forall n. KnownNat n => SomeSorted (Sorted n a)
 
 deriving instance Show a => Show (SomeSorted a)
+
+someSortedMin :: forall n m a. (KnownNat n, KnownNat m) => Sorted (Min n m) a -> SomeSorted a
+someSortedMin = case cmpNat (Proxy @n) (Proxy @m) of
+  LTI -> SomeSorted
+  EQI -> SomeSorted
+  GTI -> SomeSorted
+
+popSomeSorted :: Sorted n a -> (Entry n a, Maybe (SomeSorted a))
+popSomeSorted xs = popSorted xs (,Nothing) (\x ys -> (x, Just (SomeSorted ys)))
 
 insertionSort :: forall a. NonEmpty (Natural, a) -> SomeSorted a
 insertionSort ((k0, x0) :| xs0) = withSomeSNat k0 \(SNat @k) -> go xs0 (SomeSorted (SSingle (Entry @k x0)))
@@ -126,9 +129,10 @@ insertionSort ((k0, x0) :| xs0) = withSomeSNat k0 \(SNat @k) -> go xs0 (SomeSort
     go :: [(Natural, a)] -> SomeSorted a -> SomeSorted a
     go [] = id
     go ((k, x) : xs) = \case
-      SomeSorted @_ @n ys -> withSomeSNat k \(SNat @k) -> go xs case decideInsert @k @n of
-        DIHere -> SomeSorted $ insertSorted (Entry @k x) ys
-        DIThere -> SomeSorted $ insertSorted (Entry @k x) ys
+      SomeSorted @_ @n ys -> withSomeSNat k \(SNat @k) ->
+        go xs $
+          someSortedMin @k @n $
+            insertSorted (Entry @k x) ys
 
 main :: IO ()
 main = print $ insertionSort ((4, 'a') :| [(3, 'b'), (5, 'c'), (4, 'd')])
